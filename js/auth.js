bce3f14fd4446c3255bfcfd1d9ab574c7483a6d0
bayucy882-gif <bayucy882@gmail.com>
2026-08-02 12:32:05 +0700

    Add login function with authentication alert

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,13 @@
+alert("auth.js berhasil dimuat");
+function login(){
 
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if(username=="admin" && password=="12345"){
+        window.location.href="dashboard.html";
+    }else{
+        alert("Username atau Password salah!");
+    }
+
+}
